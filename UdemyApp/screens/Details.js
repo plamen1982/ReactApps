67b57b42550a08f9774594813bac168c10691941
plamen1982 <plamen.hristov@ymail.com>
2017-10-21 12:14:58 -0400

    Udemy App first commit

---
+++ UdemyApp/screens/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Details extends Component {
+    render() {
+        return(
+            <View 
+             style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+             <Text> Details View </Text>
+            </View>
+
+            )
+        }
+    }
+export default Details;

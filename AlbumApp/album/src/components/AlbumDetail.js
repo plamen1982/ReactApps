bf755955d3270ce81504d0af05e98e5e169da500
plamen1982 <plamen.hristov@ymail.com>
2017-11-11 17:56:45 -0500

    Styling the AlbumDetail component

--- AlbumApp/album/src/components/AlbumDetail.js
+++ AlbumApp/album/src/components/AlbumDetail.js
@@ -4,20 +4,35 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 // destructing argument object album inside AlbumDetail component
-const AlbumDetail = ({ album }) => {
+const AlbumDetail = ({ recordAlbum }) => {
 // destructing title, artist and thumbnail_image from album    
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = recordAlbum;
+    const { 
+        thumbnailStyle, 
+        headerContentStyle, 
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle,
+    } = styles; 
     return (
         <Card>
             <CardSection>
-                <View>
-                    <Image source={{ uri: thumbnail_image }} />
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
                 </View>
-                <View style={styles.headerContentStyle}>
-                    <Text>{title}</Text>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>                    
                 </View>
-
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={imageStyle} 
+                    source={{ uri: image }}
+                />
             </CardSection>
         </Card>
     );
@@ -27,6 +42,24 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50, 
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
     }
 };
 

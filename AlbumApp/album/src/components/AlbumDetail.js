613861fa9cedb3a6581a2c5bd165f0cc74e60a1b
plamen1982 <plamen.hristov@ymail.com>
2017-11-11 17:22:40 -0500

    Destructing argument object album inside the AlbumDetail component

--- AlbumApp/album/src/components/AlbumDetail.js
+++ AlbumApp/album/src/components/AlbumDetail.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// destructing argument object album inside AlbumDetail component
+const AlbumDetail = ({ album }) => {
+// destructing title, artist and thumbnail_image from album    
+    const { title, artist, thumbnail_image } = album;
     return (
         <Card>
             <CardSection>
-                <View></View>
+                <View>
+                    <Image source={{ uri: thumbnail_image }} />
+                </View>
                 <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>                    
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>                    
                 </View>
 
             </CardSection>

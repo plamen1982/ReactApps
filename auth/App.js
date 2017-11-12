64de363d4f58965c49da7812451e149162a84c77
“[plamen1982]” <plamen.hristov@ymail.com>
2017-11-11 20:24:59 -0500

    Reusing components from AlbumApp

--- auth/App.js
+++ auth/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './components/common'
 
 export default class App extends React.Component {
   render() {
     return (
       <View >
+        <Header  headerText = "Authentication"/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>

b34fb4b79793f7e654c50466f071a6a9d5755b0c
“[plamen1982]” <plamen.hristov@ymail.com>
2017-11-13 14:05:18 -0500

    Created reusable Input component

--- auth/App.js
+++ auth/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+// import firebase from 'react-native-firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 export default class App extends React.Component {
+  
+  // componentWillMount(){
+  //    firebase.initializeApp({
+  //     apiKey: 'AIzaSyCqOmf5OHuI7h9ZDmOniSmspqe7BSPFtOw',
+  //     authDomain: 'authentication-7c212.firebaseapp.com',
+  //     databaseURL: 'https://authentication-7c212.firebaseio.com',
+  //     projectId: 'authentication-7c212',
+  //     storageBucket: 'authentication-7c212.appspot.com',
+  //     messagingSenderId: '183675921688'
+  //   });
+  // }
+
   render() {
     return (
       <View >
         <Header  headerText = "Authentication"/>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <LoginForm />
       </View>
     );
   }

40f405974964ab7cc916a90cbd63693ae94cfda1
“[plamen1982]” <plamen.hristov@ymail.com>
2017-12-05 10:05:03 -0500

    Created additional screen for Create new employee

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm  from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
-
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return(
@@ -11,7 +11,18 @@ const RouterComponent = () => {
                 <Scene key="login" component={ LoginForm } title="Please Login" />
             </Scene>
             <Scene key="main">
-                <Scene key="employeeList" component= {EmployeeList} title="Employees" />
+                <Scene 
+                    onRight={()=> Actions.employeeCreate()}
+                    rightTitle="Add"
+                    key="employeeList" 
+                    component= {EmployeeList} 
+                    title="Employees" 
+                />
+                <Scene  
+                    key="employeeCreate"
+                    title="Create employee"
+                    component={EmployeeCreate}
+                />
             </Scene>
         </Router>
     )

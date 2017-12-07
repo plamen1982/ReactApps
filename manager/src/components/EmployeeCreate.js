4588949721489a9e72df2a9e3e917ad532fca74b
“[plamen1982]” <plamen.hristov@ymail.com>
2017-12-07 13:20:14 -0500

    Adding Picker and styles overrides

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import  { employeeUpdate } from '../actions';
 import { Card, CardSection, Button, Input } from './common'
@@ -25,7 +26,21 @@ class EmployeeCreate extends Component {
                     />
                 </CardSection>
 
-                <CardSection>
+                <CardSection style={{flexDirection: 'row'}}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
+                    <Picker
+                        style={{flex:1}}
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({ prop:'shift', value })}
+                    >
+                        <Picker.Item label ="Monday" value="Monday" />
+                        <Picker.Item label ="Tuesday" value="Tuesday" />
+                        <Picker.Item label ="Wednesday" value="Wednesday" />
+                        <Picker.Item label ="Thursday" value="Thursday" />
+                        <Picker.Item label ="Friday" value="Friday" />
+                        <Picker.Item label ="Saturday" value="Saturday" />
+                        <Picker.Item label ="Sunday" value="Sunday" />                        
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
@@ -37,9 +52,16 @@ class EmployeeCreate extends Component {
         );
     }
 }
+
+styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+    }
+}
  
 const mapStateToProps = (state) => {
-    console.log(state)
+
     const { name, phone, shift } = state.employeeForm;
 
     return { name, phone, shift }

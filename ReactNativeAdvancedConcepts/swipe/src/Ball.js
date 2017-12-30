eb8d55b03bbe24756647fb697460f7af420954c9
“[plamen1982]” <plamen.hristov@ymail.com>
2017-12-30 14:43:34 -0500

    How animations work

--- ReactNativeAdvancedConcepts/swipe/src/Ball.js
+++ ReactNativeAdvancedConcepts/swipe/src/Ball.js
@@ -3,15 +3,19 @@ import { View, Animated } from 'react-native';
 
 class Ball extends Component {
     componentWillMount() {
+//1. Where the item is right now? -> Animated.ValueXY(0, 0) - the initial position is x: 0, y: 0
         this.position = new Animated.ValueXY(0, 0)
         Animated.spring(this.position, {
+//2. Where we moving elment to? Animated.spring: change this.position to values x: 200, y: 500
             toValue: { x: 200, y: 500 }
         }).start();
     }
 
-
     render() {
         return(
+//3. Which element are we moving Animated.View style={this.position.getLayout()}, 
+//style is taking this.position properties to create the animation
+            
         <Animated.View style={this.position.getLayout()}>
             <View style={styles.ball} />
         </Animated.View>

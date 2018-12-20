29e517394e7b80e319ae8fb30b438ed3a883316d
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-20 21:43:12 +0200

    Adding ImageBackground and styles changes

--- wheather-app/App.js
+++ wheather-app/App.js
@@ -1,57 +1,71 @@
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
+  View,
+  ImageBackground,
   Text,
   KeyboardAvoidingView,
   Platform,
-} from "react-native";
+} from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
-  onChange() {
-    console.log('fuck yeah!')
-  }
   render() {
-    //render() method is the only required method for a React Native component.
     return (
-      <KeyboardAvoidingView style={styles.container} behavior='padding'>
-        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-        <SearchInput placeholder="Search any city"></SearchInput>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              San Francisco
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   textStyle: {
-    textAlign: "center",
-    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto"
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
-    fontSize: 44
+    fontSize: 44,
   },
   smallText: {
-    fontSize: 18
+    fontSize: 18,
   },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
-    },
 });

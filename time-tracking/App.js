f2f8a09429cc2c818b858b8844b209730d095d55
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-23 16:54:12 +0200

    foreach EditableTimer

--- time-tracking/App.js
+++ time-tracking/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import uuidv4 from 'uuid/v4';
 
 import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
 
 export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: uuidv4(),
+        elapsed: 5456099,
+        isRunning: true,
+        },
+        {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuidv4(),
+        elapsed: 1273998,
+        isRunning: false,
+        },
+    ]
+  }
+
   render() {
+    const { timers } = this.state; 
     return (
       <View style={styles.appContainer}>
           <View style={styles.titleContainer}>
@@ -13,20 +34,17 @@ export default class App extends React.Component {
           </View>
           <ScrollView style={styles.timerList}>
             <ToggleableTimerForm isOpen={true} />
-            <EditableTimer 
-              id="1"
-              title="Mow the lawn"
-              project="House Chores"
-              elapsed="8986300"
-              isRunning
-            />
-            <EditableTimer 
-              id="2"
-              title="Bake squash"
-              project="Kitchen Chores"
-              elapsed="3890985"
-              editFormOpen
-            />
+            {timers.map(({ id, title, project, elapsed, isRunning }) =>  (
+              <EditableTimer 
+                key={id}
+                id={id}
+                title={title}
+                project={project}
+                elapsed={elapsed}
+                isRunning={isRunning}
+              />
+            ))}
+
           </ScrollView>
       </View>
     );

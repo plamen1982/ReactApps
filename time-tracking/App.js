c4f711f39459f6547b6d7581ef173df27e73ac94
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-23 10:22:53 +0200

    Imported and mounted ToggleableTimerForm and EditableTimer

--- time-tracking/App.js
+++ time-tracking/App.js
@@ -10,6 +10,23 @@ export default class App extends React.Component {
           <View style={styles.titleContainer}>
             <Text style={styles.title}>Timers</Text>
           </View>
+          <ScrollView style={styles.timerList}>
+            {/* <ToggleableTimerForm isOpen={false} /> */}
+            {/* <EditableTimer 
+              id="1"
+              title="Mow the lawn"
+              project="House Chores"
+              elapsed="8986300"
+              isRunning
+            />
+            <EditableTimer 
+              id="2"
+              title="Bake squash"
+              project="Kitchen Chores"
+              elapsed="3890985"
+              editFormOpen
+            /> */}
+          </ScrollView>
       </View>
     );
   }

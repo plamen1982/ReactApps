241e8482fcb2e48387cb3173c5df5d450f9e3271
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-23 17:04:28 +0200

    Converted EditableTimer from functional component to class component

--- time-tracking/components/EditableTimer.js
+++ time-tracking/components/EditableTimer.js
@@ -7,24 +7,26 @@ import Timer from './Timer';
 //- 1. props are defined here; 2. they are not changed 
 //from other state or props and; 3. also they changed over time.
 
-export default function EditableTimer({
-    id,
-    title,
-    project,
-    elapsed,
-    isRunning,
-    editFormOpen,
-}) {
-    if(editFormOpen) {
-        return <TimerForm id={id} title={title} project={project} />
+export default class EditableTimer extends React.Component {
+    state = {
+        editFormOpen: false,
     }
 
-    return(
-    <Timer
-        id={id}
-        title={title}
-        project={project}
-        elapsed={elapsed}
-        isRunning={isRunning}
-    />);
+    render() {
+        const { id, title, project, elapsed, isRunning } = this.props;
+        const { editFormOpen } = this.state;
+
+        if(editFormOpen) {
+            return <TimerForm id={id} title={title} project={project} />
+        }
+    
+        return(
+        <Timer
+            id={id}
+            title={title}
+            project={project}
+            elapsed={elapsed}
+            isRunning={isRunning}
+        />);
+    }
 }

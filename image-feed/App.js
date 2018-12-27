15a11c122e0e79da4c831a888f3b8f87f0802f00
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-27 12:42:21 +0200

    Created Card component with Image

--- image-feed/App.js
+++ image-feed/App.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
 
-import AuthorRow from './components/AuthorRow';
+import Card from './components/Card';
 
 export default class App extends React.Component {
   render() {
     return (
-
       <View style={styles.container}>
-        <AuthorRow
+        <Card 
           fullname={'Plamen Hristov'}
           linkText={'Comments'}
-          onPressLinkText={() => {
-          console.log('Pressed link!');
-        }}
+          onPressLinkText={() => { console.log('Pressed link!'); }}
+          image={{ uri: 'https://unsplash.it/600/600' }}
         />
+
       </View>
     );
   }
---
+++ image-feed/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+import AuthorRow from './AuthorRow';
+
+export default class Card extends React.Component {
+    static propTypes = {
+        fullname: PropTypes.string.isRequired,
+        image: Image.propTypes.source.isRequired,
+        linkText: PropTypes.string,
+        onPressLinkText: PropTypes.func,
+    };
+
+    static defaultProps = {
+        linkText: '',
+        onPressLinkText: () => {},
+    };
+
+    render() {
+        const { fullname, image, linkText, onPressLinkText } = this.props;
+        return(
+            <View>
+                <AuthorRow
+                    fullname={fullname}
+                    linkText={linkText}
+                    onPressLinkText={onPressLinkText}
+            />
+                <Image style={styles.image} source={image}/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    image: {
+        aspectRatio: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.02)',
+    }
+});

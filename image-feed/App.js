dbe77cfa452b61f751881923ecabb2485fbe6f46
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-27 16:58:34 +0200

    Created feed screen with infinitely-scrolling image feed

--- image-feed/App.js
+++ image-feed/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import { Constants } from 'expo';
 
-import CardList from './components/CardList';
+import Feed from './screens/Feed';
 
 const data = [
   { id: 0, author: "Bob Ross" },
@@ -11,22 +11,27 @@ const data = [
 
 export default class App extends React.Component {
   render() {
-
     return (
       <View style={styles.container}>
-        <CardList
-          items={data}
-        />
-
+        <Feed style={styles.feed} />
       </View>
     );
   }
 }
 
+const platformVersion = 
+  Platform.OS === 'ios' ? parseInt(Platform.Version, 10) : Platform.Version;
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
+  },
+  feed: {
+    flex: 1,
+    marginTop: 
+      Platform.OS === 'android' || platformVersion < 11 
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
---
+++ image-feed/screens/Feed.js
@@ -0,0 +1,69 @@
+// This screen will fetch live data from https://unsplash.it and pass the data into our CardList .
+import React from 'react';
+import {
+    ActivityIndicator,
+    Text,
+    ViewPropTypes,
+    SafeAreaView,
+} from 'react-native';
+
+import PropTypes from 'prop-types';
+import { fetchImages } from '../utils/api';
+import CardList from '../components/CardList';
+
+export default class Feed extends React.Component {
+    static propTypes = {
+        style: ViewPropTypes.style,
+    };
+
+    static defaultProps = {
+        style: null,
+    };
+
+    state = {
+        loading: true,
+        error: false,
+        items: [],
+    };
+
+    async componentDidMount() {
+        try {
+            const items = await fetchImages();
+
+            this.setState({
+                loading: false,
+                items,
+            });
+
+        }  catch(e) {
+            this.setState({
+                loading: false,
+                error: true,
+            })
+        }
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return this.props.linkText !== nextProps.linkText;
+    }
+
+    render() {
+        const { style } = this.props;
+        const { loading, error, items } = this.state;
+
+        if(loading) {
+            return <ActivityIndicator size='large'/>
+        }
+
+        if(error) {
+            return <Text>Error...</Text>;
+        }
+
+        return (
+            <SafeAreaView style={style}>
+                <CardList items={items}/>
+            </SafeAreaView>
+        );
+
+    }
+}

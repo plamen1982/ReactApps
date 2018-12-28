f9100429b4bdc5143e5aaa95bdb904a21f6da6fe
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-28 14:21:59 +0200

    Added Modal with Comments in App.js

--- image-feed/App.js
+++ image-feed/App.js
@@ -1,53 +1,77 @@
-import React from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
-import { Constants } from 'expo';
+import React from "react";
+import { Platform, StyleSheet, View, Modal } from "react-native";
+import { Constants } from "expo";
 
-import Feed from './screens/Feed';
+import Feed from "./screens/Feed";
+import Comments from "./screens/Comments";
 
 export default class App extends React.Component {
-
   state = {
     commentsForItem: {},
     showModal: false,
-    selectedItemId: null,
+    selectedItemId: null
   };
 
   openCommentScreen = id => {
     this.setState({
       showModal: true,
-      selectedItemId: id,
+      selectedItemId: id
     });
-  }
+  };
 
   closeCommentScreen = () => {
     this.setState({
       showModal: false,
-      selectedItemId: null,
+      selectedItemId: null
     });
-  }
+  };
 
   render() {
+    const { commentsForItem, showModal, selectedItemId } = this.state;
     return (
       <View style={styles.container}>
-        <Feed style={styles.feed} />
+        <Feed
+          style={styles.feed}
+          commentsForItem={commentsForItem}
+          onPressComments={this.openCommentScreen}
+        />
+        <Modal
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen}
+        >
+          <Comments
+            style={styles.container}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+          />
+        </Modal>
+        />
       </View>
     );
   }
 }
 
-const platformVersion = 
-  Platform.OS === 'ios' ? parseInt(Platform.Version, 10) : Platform.Version;
+const platformVersion =
+  Platform.OS === "ios" ? parseInt(Platform.Version, 10) : Platform.Version;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   feed: {
     flex: 1,
-    marginTop: 
-      Platform.OS === 'android' || platformVersion < 11 
+    marginTop:
+      Platform.OS === "android" || platformVersion < 11
         ? Constants.statusBarHeight
-        : 0,
+        : 0
   },
+  comments: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "ios" && platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0
+  }
 });

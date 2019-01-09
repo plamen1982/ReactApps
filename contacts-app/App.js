287c5d151d72bb836f0217667866904a27e43daa
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-09 23:12:33 +0200

    Created Nested Navigations Wraped in TabNavigator

--- contacts-app/App.js
+++ contacts-app/App.js
@@ -3,17 +3,6 @@ import { StyleSheet } from 'react-native';
 
 import AppNavigator from './routes';
 
-export default class App extends React.Component {
-  render() {
-    return <AppNavigator />
-  }
+export default function App() {
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- contacts-app/routes.js
+++ contacts-app/routes.js
@@ -1,28 +1,96 @@
 import React from "react";
-import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import Contacts from "./screens/Contacts";
 import Profile from "./screens/Profile";
 import Favorites from "./screens/Favorites";
 import User from "./screens/User";
 
-const AppNavigator = createBottomTabNavigator(
-{
-  Contacts: {
-    screen: Contacts,
-  },
+import colors from "./utils/colors";
 
-//   Profile: {
-//     screen: Profile,
-//   },
-  Favorites: {
-      screen: Favorites,
-  },
-  User: {
-      screen: User,
-  }
-},
-
+const getTabBarIcon = icon => ({ tintColor }) => (
+  <MaterialIcons name={icon} size={26} style={{ color: tintColor }} />
 );
 
-export default createAppContainer(AppNavigator);
+const ContactsScreen = createStackNavigator(
+  {
+    Contacts: {
+      screen: Contacts
+    },
+    Profile: {
+      screen: Profile
+    }
+  },
+  {
+    initialRouteName: "Contacts",
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon("list")
+    }
+  }
+);
+
+const FavoritesScreens = createStackNavigator(
+  {
+    Favorites: {
+      screen: Favorites
+    },
+    Profile: {
+      screen: Profile
+    }
+  },
+  {
+    initialRouteName: "Favorites",
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon("star")
+    }
+  }
+);
+
+const UserScreens = createStackNavigator(
+  {
+    User: {
+      screen: User
+    }
+  },
+  {
+    initialRouteName: "User",
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon("person")
+    }
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Contacts: {
+      screen: ContactsScreen
+    },
+    Favorites: {
+      screen: FavoritesScreens
+    },
+    User: {
+      screen: UserScreens
+    }
+  },
+  {
+    initialRouteName: "Contacts",
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight
+      },
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark,
+      renderIndicator: () => null
+    }
+  }
+);
+
+export default createAppContainer(TabNavigator);
